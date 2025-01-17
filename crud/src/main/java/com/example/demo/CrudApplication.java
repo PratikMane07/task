c55c5e454ad8007c0663dabe31d4938c0061f859
Navnath3864 <nagutte0@gmail.com>
2025-01-17 16:50:21 +0530

    Merge branch 'main' of https://github.com/PratikMane07/task.git into main

--- crud/src/main/java/com/example/demo/CrudApplication.java
+++ crud/src/main/java/com/example/demo/CrudApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudApplication {
-	
 	public void m1()
 	{
 		System.out.println("this is m1");
@@ -14,13 +13,14 @@ public class CrudApplication {
 	public void m2() {
 		System.out.println("this is m2");
 	}
-
+	
 	public void m3() {
 		System.out.println("this is m3");
 	}
 	public void m4() {
 		System.out.println("this is m4");
 	}
+	
 	public void m5()
 	{
 		System.out.println("This is M5 method");
